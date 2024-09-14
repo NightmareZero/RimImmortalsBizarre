1d5fd68901754192284927597ae0507b2dbb65eb
Nightmare <nightzhuxy@hotmail.com>
2024-09-14 18:27:32 +0800

    添加

---
+++ Source/Abilities/Common/interf.cs
@@ -0,0 +1,18 @@
+using Verse;
+using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using WhoXiuXian.Abilities;
+
+using Core;
+using WhoXiuXian;
+
+
+namespace NzRimImmortalBizarre
+{ 
+    public interface IsFailable
+    {
+        bool IsCastingSuccess();
+    }
+}
