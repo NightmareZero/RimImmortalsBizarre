3bbb7ea1687a715a6edc1b4168a9cc55e7e6fab1
Nightmare <nightzhuxy@hotmail.com>
2024-09-20 19:10:07 +0800

    坐忘道   巧言令色  添加一些基础代码

--- Source/Abilities/ZuoWang/CompAbilityEffect_Zw_MvIllness.cs
+++ Source/Abilities/ZuoWang/CompAbilityEffect_Zw_MvIllness.cs
@@ -4,12 +4,43 @@ using NzRimImmortalBizarre;
 
 namespace NzRimImmortalBizarre
 {
-    public class CompAbilityEffect_Zw_MvIllness : CompAbilityEffect
+    public class CompAbilityEffect_Zw_MvIllness : CompAbilityEffect_WithDest
     {
         public new CompProperties_Zw_MvIllness Props => (CompProperties_Zw_MvIllness)props;
 
-        private Pawn Caster => parent.pawn;
+        public override bool Valid(LocalTargetInfo target, bool throwMessages = false)
+        {
+            if (!base.Valid(target, throwMessages))
+            {
+                return false;
+            }
+            if (target.Pawn == null)
+            {
+                if (throwMessages)
+                {
+                    Messages.Message("AbilityInvalidTarget".Translate(), MessageTypeDefOf.RejectInput);
+                }
+                return false;
+            }
+            return true;
+        }
 
-
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            if (target.Pawn == null)
+            {
+                
+                return;
+            }
+            // 目标地点
+            LocalTargetInfo extraTarget = this.selectedTarget;
+            if (extraTarget == null || extraTarget.Pawn == null)
+            {
+                // 如果没有指定目标地点，或者目标地点没有人，直接消息错误
+                return;
+            }
+            
+            
+        }
     }
 }
--- Source/Abilities/ZuoWang/CompProperties_Zw_MvIllness.cs
+++ Source/Abilities/ZuoWang/CompProperties_Zw_MvIllness.cs
@@ -6,12 +6,15 @@ using System.Linq;
 
 namespace NzRimImmortalBizarre
 {
-    public class CompProperties_Zw_MvIllness : CompProperties_AbilityEffect
+    public class CompProperties_Zw_MvIllness :  CompProperties_EffectWithDest
     {
 
         public CompProperties_Zw_MvIllness()
         {
             compClass = typeof(CompAbilityEffect_Zw_MvIllness);
+            destination = AbilityEffectDestination.Selected; // 选择目标
+            requiresLineOfSight = true; // 需要视线
+            
         }
     }
 }
