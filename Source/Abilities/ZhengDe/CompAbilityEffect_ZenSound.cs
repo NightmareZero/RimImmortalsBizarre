0abb4cebca2f15a5abfbbcbd53239871473d616b
Nightmare <nightzhuxy@hotmail.com>
2024-11-26 16:04:56 +0800

    正德寺   添加一些Mote

---
+++ Source/Abilities/ZhengDe/CompAbilityEffect_ZenSound.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using NzRimImmortalBizarre;
+using System.Linq;
+
+namespace NzRimImmortalBizarre
+{
+    public class CompAbilityEffect_ZenSound : CompProperties_AbilityEffect { 
+
+    }
+}
---
+++ Source/Things/Mote/Mote_ZenSound.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using NzRimImmortalBizarre;
+using System.Linq;
+
+namespace NzRimImmortalBizarre
+{
+    public class Mote_ZenSound : Mote
+    { 
+        // MoteThrown
+        // MoteAttached
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+
+            // 初始化后续逻辑
+        }
+    }
+}
