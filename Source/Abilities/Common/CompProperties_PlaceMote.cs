321a73641c1c0d8a9f48702ba3ecf1b7095f893a
Nightmare <nightzhuxy@hotmail.com>
2024-11-27 11:23:19 +0800

    添加一个通用的在指定地点放置Mote的AbilityComp // TODO

---
+++ Source/Abilities/Common/CompProperties_PlaceMote.cs
@@ -0,0 +1,17 @@
+using RimWorld;
+using System;
+using System.Linq;
+using Verse;
+
+
+
+namespace NzRimImmortalBizarre
+{ 
+    public class CompProperties_PlaceMote : CompProperties_AbilityEffect
+    {
+        public CompProperties_PlaceMote()
+        {
+            compClass = typeof(CompAbilityEffect_PlaceMote);
+        }
+    }
+}
