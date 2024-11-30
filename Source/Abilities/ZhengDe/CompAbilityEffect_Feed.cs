06352026c1d6bfec096470de5c19a973a1dbac89
Nightmare <nightzhuxy@hotmail.com>
2024-11-30 16:28:10 +0800

    正德寺   调整一些东西

---
+++ Source/Abilities/ZhengDe/CompAbilityEffect_Feed.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using NzRimImmortalBizarre;
+using System.Linq;
+
+namespace NzRimImmortalBizarre
+{
+    public class CompAbilityEffect_Feed : CompProperties_AbilityEffect { 
+
+    }
+}
