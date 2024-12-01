4490e13276e9f6e7b6443e11407ae557cf0d0633
Nightmare <nightzhuxy@hotmail.com>
2024-12-01 16:35:32 +0800

    1

--- Source/Abilities/ZhengDe/CompAbilityEffect_Feed.cs
+++ Source/Abilities/ZhengDe/CompAbilityEffect_Feed.cs
@@ -17,14 +17,28 @@ namespace NzRimImmortalBizarre
             Pawn pawn = target.Pawn;
             if (pawn != null)
             {
+                // 补满饥饿
                 if (pawn.needs.food != null)
                 {
                     pawn.needs.food.CurLevel = pawn.needs.food.MaxLevel;
                 }
 
+                // 补满睡眠
                 if (pawn.needs.rest != null)
                 {
                     pawn.needs.rest.CurLevel = pawn.needs.rest.MaxLevel;
+                }
+
+                // 将目标的成瘾品需求补满
+                if (pawn.story != null)
+                {
+                    foreach (var hediff in pawn.health.hediffSet.hediffs)
+                    {
+                        if (hediff.def.IsAddiction)
+                        {
+                            hediff.Severity = 0;
+                        }
+                    }
                 }
             }
         }
