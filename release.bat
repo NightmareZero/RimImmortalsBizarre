f99824d3345552d54b94e8ba953d267cc6bbfea3
Nightmare <nightzhuxy@hotmail.com>
2025-07-08 09:41:07 +0800

    更新release.bat脚本，添加对1.6版本的支持，并清理旧版本的临时文件

--- release.bat
+++ release.bat
@@ -11,7 +11,18 @@ MD ..\RimImmortalsBizarre-Release
 
 REM copy new mod files and directories
 xcopy /E /I /Y .\1.5 ..\RimImmortalsBizarre-Release\1.5
+xcopy /E /I /Y .\1.6 ..\RimImmortalsBizarre-Release\1.6
 xcopy /E /I /Y .\About ..\RimImmortalsBizarre-Release\About
 xcopy /E /I /Y .\Languages ..\RimImmortalsBizarre-Release\Languages
 xcopy /E /I /Y .\Sounds ..\RimImmortalsBizarre-Release\Sounds
 xcopy /E /I /Y .\Textures ..\RimImmortalsBizarre-Release\Textures
+
+
+rmdir /S /Q ..\RimImmortalsBizarre-Release\1.5\obj
+rmdir /S /Q ..\RimImmortalsBizarre-Release\1.5\Source
+rmdir /S /Q ..\RimImmortalsBizarre-Release\1.6\obj
+rmdir /S /Q ..\RimImmortalsBizarre-Release\1.6\Source
+DEL /Q /S ..\RimImmortalsBizarre-Release\1.5\Assemblies\*.pdb
+DEL /Q /S ..\RimImmortalsBizarre-Release\1.6\Assemblies\*.pdb
+DEL /Q /S ..\RimImmortalsBizarre-Release\1.5\mod.csproj
+DEL /Q /S ..\RimImmortalsBizarre-Release\1.6\mod.csproj
